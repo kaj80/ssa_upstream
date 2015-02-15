6a6a6b29071cdcea9e52328d93459e2a0f878995
Sasha Kotchubievsky <sashakot@mellanox.com>
2015-02-15 14:23:24 +0200

    Add make for all SSA projects and libraries

---
+++ autogen.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+libmlnx4/autogen.sh
+
+set -x
+test -d ./config || mkdir ./config
+aclocal -I config
+libtoolize --force --copy
+autoheader
+automake --foreign --add-missing --copy
+autoconf
