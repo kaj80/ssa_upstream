a4cc77c780809544d43cca65f4f86f0a1e7f3d1b
Sasha Kotchubievsky <sashakot@mellanox.com>
2015-03-01 18:23:13 +0200

    Add explicit call for autogen scripts

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,18 @@
 #! /bin/sh
 
-libmlnx4/autogen.sh
+libmlx4/autogen.sh
+libmlx5/autogen.sh
+libibumad/autogen.sh
+libibverbs/autogen.sh
+librdmacm/autogen.sh
+opensm/autogen.sh
+ompi/autogen.sh
+ibssa2/autogen.sh
+#ibssa2/distrib/autogen.sh
+#ibssa2/tests/autogen.sh
+#ibssa2/acm/autogen.sh
+#ibssa2/plugin/autogen.sh
+
 
 set -x
 test -d ./config || mkdir ./config
